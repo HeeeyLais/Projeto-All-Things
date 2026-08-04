ef74cb94bf69608f05181f2da106abf2249686a1
Lais <plaisaparecida@gmail.com>
2026-08-04 20:44:29 -0300

    fazendo o produto ser salvavel - formularios, etc

--- src/main/java/br/com/allthings/allthings/controller/ProdutoController.java
+++ src/main/java/br/com/allthings/allthings/controller/ProdutoController.java
@@ -1,12 +1,17 @@
 package br.com.allthings.allthings.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.allthings.allthings.entity.Produto;
+import br.com.allthings.allthings.service.ProdutoService;
 
 @Controller
 @RequestMapping("/produtos")
@@ -21,4 +26,21 @@ public class ProdutoController {
         ProdutoService.save(produto);
         return "redirect:/produtos/listar";
     }
+
+    // listar todos os produtos
+    @GetMapping("/listar")
+    public String listar(Model model){
+        List<Produto> produtos = ProdutoService.findAll();
+        model.addAttribute("produtos", produtos);
+        return "produto/listaProdutos";
+    }
+
+    // abrir formulário
+    @GetMapping("/criar")
+    public String criarForm(Model model){
+        model.addAttribute("produto", new Produto());
+        return "produto/formularioProduto";
+    }
+
+
 }
